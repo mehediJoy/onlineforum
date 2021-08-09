caa03ffc9fac55d09ea3fe088b5b6eea08db72e9
mehediJoy <mhjoy1998@gmail.com>
2021-08-09 13:45:28 +0600

    1st Phase

---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>ass</h1>
+</template>
+
+<script>
+export default {
+    name: "Home",
+    props: { 
+        subjects: [],
+    },
+    components: {
+
+    },
+    methods: {
+        
+    },
+}
+</script>
